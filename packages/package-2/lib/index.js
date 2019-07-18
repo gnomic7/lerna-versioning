826bf44ce5ac6e7afb44dbdbe6440ddc30edf6da
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-18 15:06:05 -0400

    feat: This is a breaking change
    BREAKING CHANGE
    This change is going to update the lessThan parameter to limit

--- packages/package-2/lib/index.js
+++ packages/package-2/lib/index.js
@@ -2,6 +2,6 @@
 
 module.exports = oddNumbers;
 
-function oddNumbers(lessThan = 100) {
+const oddNumbers = (limit = 100) => {
   console.log(`Getting you the odd numbers less than ${lessThan} soon!`);
-}
+};
