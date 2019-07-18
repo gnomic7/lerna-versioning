df684629e823704a05a66982c249c9cd2a23f745
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-18 16:09:12 -0400

    feat: Remove unnecesary console.log

--- packages/package-3/lib/index.js
+++ packages/package-3/lib/index.js
@@ -5,7 +5,3 @@ module.exports = multiply;
 const multiply = (...x) => {
   return x.reduce((acc, val) => acc * val);
 };
-
-console.log(
-  `Updating dependency package to see how it updates the dependent pages`
-);
