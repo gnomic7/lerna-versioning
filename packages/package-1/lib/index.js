ee7ea17f7d48d5847e98842043c09b95b113f110
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-18 15:18:02 -0400

    fix: This commit is going to make sure that the value is int

--- packages/package-1/lib/index.js
+++ packages/package-1/lib/index.js
@@ -1,9 +1,13 @@
 "use strict";
 
 module.exports = { sum, minus };
-
+/**
+ * This is a function which would take an arbitrary number of parameters
+ * and return the sume of those as int
+ * @param  {...any} x
+ */
 const sum = (...x) => {
-  return x.reduce((acc, val) => acc + val);
+  return x.reduce((acc, val) => acc + parseInt(val));
 };
 
 const minus = (x, y) => x - y;
