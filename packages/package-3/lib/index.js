ff9d5858ded6afac17d894a17dd11e7a16848e5f
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-18 16:01:24 -0400

    chore: This is just a console.log

--- packages/package-3/lib/index.js
+++ packages/package-3/lib/index.js
@@ -5,3 +5,7 @@ module.exports = multiply;
 const multiply = (...x) => {
   return x.reduce((acc, val) => acc * val);
 };
+
+console.log(
+  `Updating dependency package to see how it updates the dependent pages`
+);
