7637d5ef1e50478e13a782a7be98cb761ea678df
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-18 15:30:13 -0400

    feat: Add dependency of package 1 in package 2

--- packages/package-2/lib/index.js
+++ packages/package-2/lib/index.js
@@ -1,7 +1,8 @@
 "use strict";
-
+const { sum, minus } = require("@gnomic7/package-1");
 module.exports = oddNumbers;
 
 const oddNumbers = (limit = 100) => {
+  console.log(sum(1, 2, 4));
   console.log(`Getting you the odd numbers less than ${lessThan} soon!`);
 };
