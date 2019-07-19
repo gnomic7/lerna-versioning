bb6b0ba73425e3de6d91643eea24227e7c003395
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-19 11:44:25 -0400

    feat: Add displayName function to commons package

--- packages/commons/index.js
+++ packages/commons/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   myTest: (x, y) => console.log(x, y),
   helloWorld: () => console.log(`Hello world`),
-  greet: (name) => console.log(`Hi, ${name}`);
+  greet: name => console.log(`Hi, ${name}`),
+  fullName: (first, last) => console.log(`${first} ${last}`)
 };
