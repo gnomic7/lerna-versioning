853900d64e542f75d6ca56802a6abd1ac4341102
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-19 12:03:02 -0400

    test: Add a few more tests

--- packages/commons/index.spec.js
+++ packages/commons/index.spec.js
@@ -3,4 +3,13 @@ describe("Commons", () => {
   test("Any this is true", () => {
     expect(1 + 1).toBe(2);
   });
+  test("Display full name when first and last name is porvided", () => {
+    expect(fullName("John", "Doe")).toBe("John Doe");
+  });
+  test("when greeted - displays appropriate greeting", () => {
+    expect(greet("John")).toBe("Hi, John");
+  });
+  test("Should display hello world message", () => {
+    expect(helloWorld()).toBe("Hello world");
+  });
 });
