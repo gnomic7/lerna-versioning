bd58deca4c1e2cc03375354abdff25c4a27e83a1
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-19 09:28:16 -0400

    feat: Add jsdoc for the package-3 function

--- packages/package-3/lib/index.js
+++ packages/package-3/lib/index.js
@@ -2,6 +2,11 @@
 
 module.exports = multiply;
 
+/**
+ * This funciton will return the product of every number passed as param
+ *
+ * @param  {...any} x any number of params
+ */
 const multiply = (...x) => {
   return x.reduce((acc, val) => acc * val);
 };
