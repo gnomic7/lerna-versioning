7ed0de14812b2c5f7e105c94a8f667f1cd02661d
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-19 09:35:42 -0400

    feat: Remove unnecessary comment block

--- packages/package-3/lib/index.js
+++ packages/package-3/lib/index.js
@@ -4,7 +4,7 @@ module.exports = multiply;
 
 /**
  * This funciton will return the product of every number passed as param
- * Will add more logic to this function in future
+ *
  * @param  {...any} x any number of params
  */
 const multiply = (...x) => {
