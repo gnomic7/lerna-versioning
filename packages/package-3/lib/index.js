6760bb207acd3b76095942bc781f01774f06b726
Rohit K Dhakal <rohit.k.dhakal@centene.com>
2019-07-19 09:58:09 -0400

    chore: This is a breaking change
    BREAKING CHANGE: multiply function accepts two params

--- packages/package-3/lib/index.js
+++ packages/package-3/lib/index.js
@@ -5,8 +5,12 @@ module.exports = multiply;
 /**
  * This funciton will return the product of every number passed as param
  *
+ * @param {any} a extra message before multiplying
  * @param  {...any} x any number of params
  */
-const multiply = (...x) => {
+const multiply = (a, ...x) => {
+  console.log(
+    `Just an extra param which will be excluded from the product: ${a}`
+  );
   return x.reduce((acc, val) => acc * val);
 };
